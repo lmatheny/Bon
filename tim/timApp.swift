0aebfb1d4ce30136db706e97c56f0996f3624eec
Luke Matheny <lmatheny277@gmail.com>
2023-07-03 11:18:35 -0400

    Initial Commit

---
+++ tim/timApp.swift
@@ -0,0 +1,17 @@
+//
+//  timApp.swift
+//  tim
+//
+//  Created by Luke Matheny on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct timApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
